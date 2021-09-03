2b07016c2493d89f9ea00d6a856bbfb7d632f768
Thiefwerty <79317665+Thiefwerty@users.noreply.github.com>
2021-09-03 14:39:40 +0300

    Refact code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[93]:
 
 
 import streamlit as st
@@ -13,16 +12,8 @@ from plotly import graph_objs as go
 from PIL import Image
 import model
 
-
-# In[85]:
-
-
 START = "2015-01-01"
 TODAY = date.today().strftime('%Y-%m-%d')
-
-
-# In[86]:
-
 
 @st.cache
 def load_data(ticker):
@@ -31,60 +22,31 @@ def load_data(ticker):
     data.Date = data.Date.apply(lambda x: x.strftime('%Y-%m-%d'))
     return data
 
-
-# In[87]:
-
-
 def plot_raw_data():
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=data['Date'], y = data['Close'], name = 'Close price'))
     fig.layout.update(title_text = 'Time Series Data', xaxis_rangeslider_visible=True, width = 900, height = 600)
     st.plotly_chart(fig,use_container_width=True)
 
-
-# In[88]:
-
-
+    
 st.title("Stock Predicition App")
-
-
-# In[89]:
-
 
 st.sidebar.title("Select Dataset, Model and Prediction period")
 
 stocks = ('AAPL', 'GOOG', 'MSFT', 'GME')
 selected_stock = st.sidebar.selectbox("Select dataset for prediction", stocks)
-# In[90]:
-
 
 data = load_data(selected_stock)
 
-
-# In[91]:
-
-
 st.subheader('Raw data')
 st.write(data.tail())
-
-
-# In[92]:
-
 
 plot_raw_data()
 
 
 # Forecasting
-
-# In[80]:
-
-
 models = ('Prophet', 'None')
 selected_model = st.sidebar.selectbox("Select model for prediction", models)
-
-
-# In[81]:
-
 
 n_years = st.sidebar.slider("Years pf prediction:", 1, 4)
 period = n_years * 365
@@ -93,21 +55,7 @@ st.sidebar.subheader('About')
 
 st.sidebar.markdown("[![Foo](https://i.ibb.co/BTzGtLg/Git-Hub-Mark-32px.png)](https://github.com/Thiefwerty)")
 
-# In[82]:
-
-
 df_train = data[['Date', 'Close']]
-
-
-# In[83]:
-
 
 model = model.Model(selected_model, df_train, period)
 model.start()
-
-
-# In[ ]:
-
-
-
-
